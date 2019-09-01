9a25da75874aca87fd25bbed07f44b9a33d4cbb2
Joshua Groover <54771092+joshua-groover@users.noreply.github.com>
2019-09-01 10:17:04 -0400

    Delete OneDimensional.java

--- OneDimensional.java
+++
@@ -1,7 +0,0 @@
-public interface OneDimensional {
-    public boolean identicalRows(int[] row1, int[] row2);
-    public boolean validateValue(int val, int maxPowerOfTwo);
-    public boolean validateRow(int[] row);
-    public boolean moveLeft(int[] row);
-    public boolean combineLeft(int[] row);
-}
